b35d67211c75af97c358849ca3459a19ac988403
chapeupreto <falajhou@gmail.com>
2015-08-09 04:03:34 -0300

    site ICE retiro/2015

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Retiro de Jovens - Igreja Cristã Evangélia Catalão">
+<meta name="author" content="rod@wgo.com.br">
+<title>Retiro de Jovens | Igreja Cristã Evangélica - Catalão/GO</title>
+
+<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<link href="assets/css/fotorama.css" rel="stylesheet">
+
+<!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="assets/css/main.css" rel="stylesheet">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+<link href="assets/css/animate-custom.css" rel="stylesheet">
+<link href="assets/css/magnific-popup.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/fotorama.js"></script>
+
+<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+<div id="navbar-main"> 
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a class="navbar-brand" href="index.php"><img src="assets/img/logotipo.png"></a> </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#home" class="smoothScroll">Home</a></li>
+          <li> <a href="#about" class="smoothScroll"> Sobre</a></li>
+          <li> <a href="#local" class="smoothScroll"> Local</a></li>
+          <li> <a href="#inscricao" class="smoothScroll"> Inscrição</a></li>
+          <li> <a href="#contato" class="smoothScroll"> Contato</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+  </div>
+</div>
+
+<!-- ==== HEADERWRAP ==== -->
+<div id="headerwrap" name="home">
+  <header class="clearfix">
+    <h1>Retiro de Jovens - eu e Cristo?</h1>
+    <p>Dias 10 a 12 de outubro/2015</p>
+    <a href="#inscricao" class="smoothScroll btn btn-lg">Inscreva-se</a> </header>
+</div>
+<!-- /headerwrap --> 
+
+<!-- ==== ABOUT ==== -->
+<div id="about" name="about">
+  <div class="container">
+    <div class="row white">
+      <div class="col-md-6"><a class="popup" href="assets/img/cartaz.png"><img class="img-responsive" src="assets/img/divulgacao.png" align=""></a></div>
+      <div class="col-md-6">
+        <h3>Sobre</h3>
+        <p>A liderança dos “Jovens em Ação” da Igreja Cristã Evangélica de Catalão promoverá nos dias <strong>10, 11 e 12 de outubro/2015</strong> mais um grandioso retiro espiritual.</p>
+        <p>O evento terá como seu preletor o <strong>Pastor Rogério</strong> da <em>Igreja Cristã Evangélica de Uberlândia</em> e acontecerá na <strong>“Chácara Elias”</strong> na cidade de <em>Três Ranchos</em>.</p>
+        <p><strong>“Eu e Cristo? Estatura Completa de Cristo”</strong> é o tema do retiro desse ano e tem como base a escritura em <em>Efésios 4:13</em>.</p>
+        <p>Dentre várias lições, o tema aborda com quem os jovens devem se parecer e aonde deve estar a sua procura diária.</p>
+        <p>O louvor contará com a participação especial do irmão <strong>Paulinho</strong> da <em>Igreja Cristã Evangélica de Campinas</em> (Goiânia/GO) juntamente com o <strong>Ministério de Louvor dos Jovens</strong> e a <strong>DC3</strong>.</p>
+        <p>Jovem, aproveite esses dias maravilhosos de comunhão e busque ao Senhor. Use sua força e sua juventude para Deus e seja um Jovem na Estatura de Cristo! Aguardamos por você!</p>
+        <blockquote>“Eu vos escrevi, jovens, porque sois fortes, e a palavra de Deus está em vós, e já vencestes o maligno” 1 João 2:14.</blockquote>
+      </div>
+    </div>
+    <!-- row --> 
+  </div>
+</div>
+<!-- container --> 
+
+<!-- ==== LOCAL ==== -->
+<div id="local" name="local">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered">Local</h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2">
+        <p class="large">O local do evento será na <strong>"Chácara Elias"</strong> situada na cidade turística de Três Ranchos/GO</p>
+        
+          <div class="fotorama"
+            id="keyboard"
+            data-keyboard="true"
+            data-loop="true"
+            data-nav="thumbs">
+              <a href="assets/img/local/large/local_1.png"><img src="assets/img/local/thumbs/local_1.jpg"></a>
+              <a href="assets/img/local/large/local_2.png"><img src="assets/img/local/thumbs/local_2.jpg"></a>
+              <a href="assets/img/local/large/local_3.png"><img src="assets/img/local/thumbs/local_3.jpg"></a>
+              <a href="assets/img/local/large/local_4.png"><img src="assets/img/local/thumbs/local_4.jpg"></a>
+              <a href="assets/img/local/large/local_5.png"><img src="assets/img/local/thumbs/local_5.jpg"></a>
+              <a href="assets/img/local/large/local_6.png"><img src="assets/img/local/thumbs/local_6.jpg"></a>
+              <a href="assets/img/local/large/local_7.png"><img src="assets/img/local/thumbs/local_7.jpg"></a>
+              <a href="assets/img/local/large/local_8.png"><img src="assets/img/local/thumbs/local_8.jpg"></a>
+              <a href="assets/img/local/large/local_9.png"><img src="assets/img/local/thumbs/local_9.jpg"></a>
+        </div>        
+        
+      </div>
+      
+    </div>
+    <!-- row --> 
+  </div>
+</div>
+<!-- container --> 
+
+<!-- ==== inscricao ==== -->
+<div id="inscricao" name="inscricao">
+  <div class="container">
+    <div class="row">
+      <h2 class="centered">Inscrição</h2>
+      <hr>
+      <div class="col-lg-8 col-lg-offset-2 centered">
+        <p class="large">O valor do retiro é de <strong>R$130,00</strong> para <em>alojamento</em> e <strong>R$120,00</strong> para <em>barraca</em>.</p>
+	    <p>Formas de pagamento à vista, 2x ou 3x. Vagas limitadas!</p>
+	    <strong>Sua inscrição somente será efetivada após a devida confirmação do pagamento.</strong>
+      </div>
+    </div>
+    <!-- /row -->
+      <!-- Change the width and height values to suit you best -->
+<div class="typeform-widget" data-url="https://jovensicec.typeform.com/to/EI1LmU" data-text="Inscrição" style="width:100%;height:500px;"></div>
+<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+    <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by <a href="http://www.typeform.com/?utm_campaign=typeform_EI1LmU&amp;utm_source=website&amp;utm_medium=typeform&amp;utm_content=typeform-embedded&amp;utm_term=Português" style="color: #999" target="_blank">Typeform</a></div>
+
+  </div>
+<!-- /container --> 
+
+<!-- ==== contato ==== -->
+<div id="contato" name="contato">
+
+
+<!-- Change the width and height values to suit you best -->
+<div class="typeform-widget" data-url="https://jovensicec.typeform.com/to/CKadsK" data-text="Contato" style="width:100%;height:641px;"></div>
+<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+    <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by <a href="http://www.typeform.com/?utm_campaign=typeform_CKadsK&amp;utm_source=website&amp;utm_medium=typeform&amp;utm_content=typeform-embedded&amp;utm_term=Português" style="color: #999" target="_blank">Typeform</a></div>
+
+  <!-- row --> 
+</div>
+<!-- container --> 
+
+<div id="footerwrap">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8"> <span class="copyright">Copyright &copy; <?=strftime('%Y'); ?> Igreja Cristã Evangélica - Catalão/GO</span> </div>
+      <div class="col-md-4">
+        <ul class="list-inline social-buttons">
+          <li><a href="https://www.facebook.com/mocidadecatalao" target="_blank"><i class="fa fa-facebook"></i></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="assets/js/retina.js"></script> 
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script> 
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script> 
+<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+<script type="text/javascript" src="assets/js/jquery.magnific-popup.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.popup').magnificPopup({type:'image'});
+});
+</script>
+</body>
+</html>
